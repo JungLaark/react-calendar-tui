466c3dab18f3ee437bef5983621bb49dd76ab79c
JCS <chenone839@gmail.com>
2021-12-10 22:42:17 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import CalendarMain from "./components/CalendarMain";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const start = new Date();
+const end = new Date(new Date().setMinutes(start.getMinutes() + 30));
+
+const sampleSchedule = [
+    {
+        id: "1",
+        calendarId: "0",
+        title: "TOAST UI Calendar Study",
+        category: "time",
+        dueDateClass: "",
+        start,
+        end,
+    },
+    {
+        id: "2",
+        calendarId: "0",
+        title: "Practice",
+        category: "milestone",
+        dueDateClass: "",
+        start,
+        end,
+        isReadOnly: true,
+    },
+    {
+        id: "3",
+        calendarId: "0",
+        title: "FE Workshop",
+        category: "allday",
+        dueDateClass: "",
+        start,
+        end,
+        isReadOnly: true,
+    },
+    {
+        id: "4",
+        calendarId: "0",
+        title: "Report",
+        category: "time",
+        dueDateClass: "",
+        start,
+        end,
+    },
+];
+
+const App = () => {
+    const [originSchedule, setOriginSchedule] = useState([sampleSchedule]);
+
+    const onDeleteSchedule = (e, callback) => {
+        console.log("onAddSchedule : " + callback);
+    };
+    const onUpdateSchedule = (e, callback) => {
+        console.log("onAddSchedule : " + callback);
+    };
+
+    const onAddSchedule = (e, schedule) => {
+        console.log("onAddSchedule : " + JSON.stringify([schedule]));
+    };
+
+    return (
+        <div>
+            <CalendarMain sampleSchedule={sampleSchedule}
+                          originSchedule={originSchedule}
+                          setOriginSchedule={setOriginSchedule}
+                          onDeleteSchedule={onDeleteSchedule}
+                          onUpdateSchedule={onUpdateSchedule} />
+                          onAddSchedule={onAddSchedule}
+        </div>
+    );
+};
 
 export default App;
---
+++ src/components/CalendarMain.js
@@ -0,0 +1,182 @@
+import React, {useState, useRef, useEffect} from 'react';
+import Calendar from '@toast-ui/react-calendar';
+import 'tui-calendar/dist/tui-calendar.css';
+import dayjs from 'dayjs';
+import './CalendarHeader.css'
+
+// If you use the default popups, use this.
+import 'tui-date-picker/dist/tui-date-picker.css';
+import 'tui-time-picker/dist/tui-time-picker.css';
+
+// const calendar = new Calendar('#calendar', {
+//     defaultView: 'month',
+//     taskView: false,
+//     useCreationPopup: true,
+//     useDetailPopup: true
+// });
+
+
+var calendar = null;
+
+const CalendarMain = (props) => {
+
+    const calendarRef = useRef();
+
+    useEffect(()=>{
+        if(calendarRef){
+            
+        calendar = calendarRef.current.getInstance();
+
+        }
+    }, [calendarRef]);
+  
+    /*header*/
+    const [currentDate, setCurrentDate] = useState(dayjs().format('YYYY MM'));
+    const [btnSelected, setBtnSelected] = useState(0);
+
+    const onClickThisMonth = () => {
+        //calendar.state.today();
+        //calendar.getInstance today(); 
+        
+        if(calendar) calendar.today();
+        setCurrentDate(dayjs(calendar.getDate()._date).format('YYYY MM'));
+    }
+
+    const onClickPrevMonth = () => {
+        if(calendar)  calendar.prev(); 
+        setCurrentDate(dayjs(calendar.getDate()._date).format('YYYY MM'));
+    }
+
+    const onClickNextMonth = () => {
+        if(calendar) calendar.next(); 
+        setCurrentDate(dayjs(calendar.getDate()._date).format('YYYY MM'));
+    }
+
+    const onClickChangeViewMonth = (e) => {
+        if(calendar) calendar.changeView("month", true);
+        setBtnSelected(e.target.id);
+    }
+
+    const onClickChangeViewWeek = (e) => {
+        if(calendar)  calendar.changeView("week", true);
+        setBtnSelected(e.target.id);
+    }
+
+    const onClickChangeViewDay = (e) => {
+        if(calendar)  calendar.changeView("day", true);
+        setBtnSelected(e.target.id);
+    }
+    /*header end*/
+
+    return (
+        <>
+         <div style={{display: 'flex', padding: '30px 30px 0 30px' }}>
+                <div style={{padding: '0 20px 0 0'}}>
+                    <button style={{backgroundColor: 'transparent', border: '1px solid #e7e7e7', borderRadius: '4px 4px 4px 4px ', height: '30px', width: '70px'}}>Calendar</button>
+                </div>
+                <div style={{padding: '0 20px 0 0'}}>
+                    <button style={{backgroundColor: 'transparent', border: '1px solid #e7e7e7', borderRadius: '4px 4px 4px 4px ', height: '30px', width: '70px'}}>Project</button>
+                </div>
+            </div>
+            <div style={{ display: "flex", margin: "30px"}}>
+                <div style={{fontWeight: 'bolder', marginTop: '3px', width: '90px'}}>
+                 {currentDate}
+                </div>
+                <button onClick={onClickPrevMonth} style={{ margin: "0 0 0 0", backgroundColor: 'transparent',  color: 'black', border: '1px solid #e7e7e7', borderRadius: '4px 0 0 4px '}}>
+                    <i className="arrow left" ></i>
+                </button>
+                <button onClick={onClickThisMonth} style={{ margin: "0 0 0 0", backgroundColor: 'transparent',  color: 'black', border: '1px solid #e7e7e7', borderRadius: '0 0 0 0' }}>
+                    Today
+                </button>
+                <button onClick={onClickNextMonth} style={{ margin: "0 20px 0 0", backgroundColor: 'transparent',  color: 'black', border: '1px solid #e7e7e7', borderRadius: '0 4px 4px 0' }}>
+                    <i className="arrow right" ></i>
+                </button>
+
+                <button id="btnMonth" onClick={onClickChangeViewMonth} style={{width: '80px', backgroundColor: 'transparent', border: '1px solid #e7e7e7', borderRadius: '4px 0 0 4px', color: 'btnMonth' === btnSelected ? "#00C1C8" : ""}}>
+                    Month
+                </button>
+                <button id="btnWeek"  onClick={onClickChangeViewWeek} style={{width: '80px', backgroundColor: 'transparent', border: '1px solid #e7e7e7', borderRadius: '0 0 0 0', color: 'btnWeek' === btnSelected ? "#00C1C8" : ""}}>
+                    Week
+                </button>
+                <button id="btnDay"  onClick={onClickChangeViewDay} style={{width: '80px', backgroundColor: 'transparent',  border: '1px solid #e7e7e7', borderRadius: '0 4px 4px 0', color: 'btnDay' === btnSelected ? "#00C1C8" : ""}}>
+                    Day
+                </button> 
+                <div className="topnav">
+                    <input type="text" placeholder="Search.."></input>
+                </div>
+{/*                 
+                <div class="dropdown" style={{float:'right', borderRadius:'5px'}}>
+                    <button class="dropbtn">All</button>
+                    <div class="dropdown-content">
+                        <a href="#">Link 1</a>
+                        <a href="#">Link 2</a>
+                        <a href="#">Link 3</a>
+                </div>
+                </div>    */}
+            </div>
+            <Calendar id='calendar' ref={calendarRef}
+                      onPrev={onClickPrevMonth}
+                      onToday={onClickThisMonth}
+                      onNext={onClickNextMonth}
+                      onChangeView={onClickChangeViewMonth}
+        height="900px"
+        calendars={[
+            {
+            id: '0',
+            name: 'Private',
+            bgColor: '#9e5fff',
+            borderColor: '#9e5fff'
+            },
+            {
+            id: '1',
+            name: 'Company',
+            bgColor: '#00a9ff',
+            borderColor: '#00a9ff'
+            }
+        ]}
+        disableDblClick={true}
+        disableClick={false}
+        isReadOnly={false}
+        month={{
+            startDayOfWeek: 0
+        }}
+        schedules={props.sampleSchedule}
+        scheduleView
+        taskView
+        template={{
+            milestone(schedule) {
+            return `<span style="color:#fff;background-color: ${schedule.bgColor};">${
+                schedule.title
+            }</span>`;
+            },
+            milestoneTitle() {
+            return 'Milestone';
+            },
+            allday(schedule) {
+            return `${schedule.title}<i class="fa fa-refresh"></i>`;
+            },
+            alldayTitle() {
+            return 'All Day';
+            }
+        }}
+        //   theme={myTheme}
+        timezones={[
+            {
+            timezoneOffset: 540,
+            displayLabel: 'GMT+09:00',
+            tooltip: 'Seoul'
+            }
+        ]}
+        useDetailPopup
+        useCreationPopup
+        view={'month'} // You can also set the `defaultView` option.
+        week={{
+            showTimezoneCollapseButton: true,
+            timezonesCollapsed: true
+        }}/>
+</>
+    );
+};
+
+
+export default CalendarMain;
